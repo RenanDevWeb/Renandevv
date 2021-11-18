43a0d8d17ac4de4cadc0a421507b530947868688
Renan <renansalmazio@gmail.com>
2021-11-18 11:40:30 -0300

    minificando arquivos

---
+++ js/datecopy.js
@@ -0,0 +1,12 @@
+const  acertarData = () => {
+  const paragrafoDeData = document.getElementById("date")
+  const data = new Date()
+  const year =  data.getFullYear()
+  paragrafoDeData.innerHTML = `Copyright ©${year} TODOS OS DIREITOS RESERVADOS`
+}
+
+
+window.onload = () =>  {
+  acertarData();
+};
+
